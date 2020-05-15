5447121f5d852157abf32421fa738dd65a26a2af
kristoffer <97krihopgmail.com>
2020-05-15 23:49:45 +0200

    added teams to active feld

--- docs/js/active.js
+++ docs/js/active.js
@@ -6,17 +6,6 @@ const activeComments = document.querySelector('[data-active-comment]');
 const activeForm = document.querySelector('[data-new-active-form]');
 const activeInput = document.querySelector('[data-new-active-input]');
 
-function rederActive() {
-	const selectedList = lists.find((list) => list.id === selectedListId);
-	if (selectedListId == null) {
-		activeDiv.style.display = 'none';
-	} else {
-		activeDiv.style.display = '';
-		activeTitle.innerText = selectedList.name;
-		rednerComments(selectedList);
-	}
-}
-
 activeForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const comment = activeInput.value;
@@ -29,14 +18,41 @@ activeForm.addEventListener('submit', (e) => {
 	saveAndRender();
 });
 
+function rederActive() {
+	const selectedList = lists.find((list) => list.id === selectedListId);
+	if (selectedListId == null) {
+		activeDiv.style.display = 'none';
+	} else {
+		activeDiv.style.display = '';
+		activeTitle.innerText = selectedList.name;
+		rednerTeams(selectedList);
+		rednerComments(selectedList);
+	}
+}
+
 function rednerComments(selectedList) {
 	clearElement(activeComments);
 
 	for (let i = 0; i < selectedList.comments.length; i++) {
-		const element = selectedList.comments[i];
 		const commentElement = document.createElement('li');
 		commentElement.classList.add('list-name');
 		commentElement.innerText = selectedList.comments[i];
 		activeComments.appendChild(commentElement);
+	}
+}
+function rednerTeams(selectedList) {
+	clearElement(activeTeams);
+
+	for (let i = 0; i < selectedList.team.length; i++) {
+		const teamElement = document.createElement('li');
+		teamElement.classList.add('team-name');
+		const teamId = selectedList.team[i];
+
+		teams.forEach((team) => {
+			if (team.id == teamId) {
+				teamElement.innerText = team.name;
+			}
+		});
+		activeTeams.appendChild(teamElement);
 	}
 }
