800d67b4deb058e80c4b845bad75f39bf8082098
Kristoffer <97krihopgmail.com>
2020-05-21 01:23:20 +0200

    added emoji

--- docs/js/task.js
+++ docs/js/task.js
@@ -2,6 +2,31 @@ const tasksDiv = document.querySelector('[data-tasks]');
 const newTaskForm = document.querySelector('[data-new-task-form]');
 const newTaskInput = document.querySelector('[data-new-task-input]');
 
+const allowedEmoji = [
+	'😊',
+	'🙃',
+	'🤪',
+	'🤓',
+	'🤯',
+	'😴',
+	'💩',
+	'👻',
+	'👽',
+	'🤖',
+	'👾',
+	'👐',
+	'🖖',
+	'✌️',
+	'🤟',
+	'🤘',
+	'🤙',
+	'👋',
+	'🐭',
+	'🦕',
+	'🦖',
+	'🐉'
+];
+
 tasksDiv.addEventListener('click', e => {
 	if (e.target.tagName.toLowerCase() === 'li') {
 		if (selectedTaskId === e.target.dataset.taskId) selectedTaskId = null;
@@ -29,7 +54,8 @@ function createTask(name) {
 		name: name,
 		team: [],
 		comments: [],
-		complete: false
+		complete: false,
+		emoji: Math.floor(Math.random() * allowedEmoji.length)
 	};
 }
 
@@ -39,7 +65,9 @@ function renderTasks() {
 		const taskElement = document.createElement('li');
 		taskElement.dataset.taskId = task.id;
 		taskElement.classList.add('task-name');
-		taskElement.innerText = task.name;
+		if (task.complete === true) taskElement.innerText = '✔' + task.name;
+		else taskElement.innerText = allowedEmoji[task.emoji] + task.name;
+
 		if (task.id === selectedTaskId) {
 			taskElement.classList.add('active-task');
 		}
